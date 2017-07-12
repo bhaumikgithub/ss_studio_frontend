021dfd3f6f681b978a5bbb6244ee33a29cb7554a
Ashvin chauhan <ashvin.tps@gmail.com>
2017-07-12 14:12:11 +0530

    [Complete] Integrate services page, [Pending] sticky footer issue

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -5,11 +5,13 @@ import NotFound from '../NotFound';
 // Import component
 import Home from '../Home';
 import GetInTouch from '../contact/GetInTouch';
+import Services from '../contact/Services';
 
 const routes = () =>
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/get_in_touch" component={GetInTouch} />
+    <Route exact path="/services" component={Services} />
     <Route component={NotFound} />
   </Switch>;
 
