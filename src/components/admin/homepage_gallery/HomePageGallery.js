7d0030844e8e9c5723d1a891123071f455dd3dcb
arps1989dev <arpita.sojitra@gmail.com>
2017-08-24 19:19:27 +0530

    update homepage photo but ui not set

--- src/components/admin/homepage_gallery/HomePageGallery.js
+++ src/components/admin/homepage_gallery/HomePageGallery.js
@@ -5,61 +5,95 @@ import { Col,Row } from 'react-bootstrap';
 import '../../../assets/css/admin/homepage_gallery/homepage-gallery.css';
 
 // Import services
-import { getActiveHomepagePhotos } from '../../../services/admin/HomePageGallery';
+import { getActiveHomepagePhotos, updateHomepagePhoto } from '../../../services/admin/HomePageGallery';
+import { getIndex } from '../../Helper';
 
 export default class HomePageGallery extends Component {
 constructor(props){
-		super(props);
-		this.state = {      
-			editObject: {},
-			active_photos: []
-		}
-	}
+    super(props);
+    this.state = {
+      active_photos: [],
+      homepage_photo: {}
+    }
+  }
 
-	componentWillMount() {
+  componentWillMount() {
     var self = this;
-
+    
     getActiveHomepagePhotos()
       .then(function(response) {
-				var data = response.data;
-				console.log(data);
+        var data = response.data;
         self.setState({ active_photos: data.data.active_photos });
+      })
+      .catch(function(error) {
+        console.log(error.response);
+      });
+      
+  }
+
+  handleUploadFile = (e, id) => {
+    e.preventDefault();
+    
+    var self = this;
+    let file = e.target.files[0];
+    let data = new FormData();
+    data.append('homepage_photo[homepage_image]', file);
+
+    updateHomepagePhoto(data,id)
+      .then(function(response) {
+        self.handleSuccessResponse(response);
       })
       .catch(function(error) {
         console.log(error.response);
       });
   }
 
-	render() {
-		const { active_photos } = this.state;
-		
-		return (
-			<Col xs={12} className="homepage-gallery-page-wrap">
-				<Row>
-						<Col xs={12} className="homepage-gallery">
-							<Col xs={12} className="slider-images-wrap">
-								{active_photos.map((active_photo, index) => 
-									<Col xs={6} sm={4} md={4} lg={3} className="gallery-slider-img-wrap" key={active_photo.id}>
-										<img className="img-responsive album-image"
-											src={active_photo.homepage_image} 
-											alt={active_photo.homepage_image_file_name} 
-											
-										/>
-										<div className="slider-num">
-											slide {index+1}
-											<a className="edit-slide">
-												<img className="img-responsive" 
-													alt=""
-													src={require("../../../assets/images/admin/homepage_gallery/img-edit-icon.png")} 
-												/>        
-											</a>
-										</div>
-									</Col>                    
-								)}
-							</Col>
-						</Col>     
-				</Row>
-			</Col>
-		);
-	}
+
+  handleSuccessResponse(response) {
+    if (response.status === 201) {
+      this.handlePhotoRendering(response);
+    }
+  }
+
+  handlePhotoRendering(response) {
+    var active_photos = this.state.active_photos;
+    var photoIndex = getIndex(response.data.data.homepage_photo.id, active_photos, 'id');
+    active_photos[photoIndex] = response.data.data.homepage_photo;
+    this.setState({ active_photos: active_photos });
+  }
+
+
+  render() {
+    const { active_photos } = this.state;
+    
+    return (
+      <Col xs={12} className="homepage-gallery-page-wrap">
+        <Row>
+            <Col xs={12} className="homepage-gallery">
+              <Col xs={12} className="slider-images-wrap">
+                {active_photos.map((active_photo, index) => 
+                  <Col xs={6} sm={4} md={4} lg={3} className="gallery-slider-img-wrap" key={active_photo.id}>
+                    <img className="img-responsive album-image"
+                      src={active_photo.homepage_image} 
+                      alt={active_photo.homepage_image_file_name} 
+                      
+                    />
+                    <div className="slider-num">
+                      slide {index+1}
+                      <a className="edit-slide">
+                        <img className="img-responsive" 
+                          alt=""
+                          src={require("../../../assets/images/admin/homepage_gallery/img-edit-icon.png")} 
+                        />        
+                        <input type="file" onChange={(e)=>this.handleUploadFile(e, active_photo.id)}/>
+                      </a>
+                    </div>
+                  </Col>                    
+                )}
+              </Col>
+            </Col>     
+        </Row>
+      </Col>
+    );
+  }
 }
--- src/services/admin/HomePageGallery.js
+++ src/services/admin/HomePageGallery.js
@@ -3,4 +3,12 @@ import { apiHeader } from '../../components/Helper';
 
 export function getActiveHomepagePhotos() {
     return axios.get(process.env.REACT_APP_API_BASE_URL + 'homepage_photos/active', apiHeader());
-  }
+}
+
+export function updateHomepagePhoto(params,id) {
+  return axios.patch(
+    process.env.REACT_APP_API_BASE_URL + 'homepage_photos/' + id,
+    params, 
+    apiHeader()
+  );
+}
