7a64158b585ec419daaed473905e409a0ca65d43
Ashvin chauhan <ashvin.tps@gmail.com>
2017-08-24 19:00:12 +0530

    Add Iframe in visitor side video film

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { PageHeader, Grid, Col, Button } from 'react-bootstrap';
-import { Player, BigPlayButton } from 'video-react';
+import Iframe from 'react-iframe';
 
 // Import services
 import { getPublishVideos } from '../services/Film';
 
-import 'video-react/dist/video-react.css';
+// Import css
 import '../assets/css/films.css';
 
 export default class Films extends Component {
@@ -22,8 +22,7 @@ export default class Films extends Component {
     getPublishVideos()
       .then(function(response) {
         var data = response.data;
-        // debugger;
-        self.setState({ videos: data.data.videos, meta: data.meta });
+        self.setState({ videos: data.data.videos });
       })
       .catch(function(error) {
         console.log(error.response);
@@ -43,53 +42,33 @@ export default class Films extends Component {
             </PageHeader>
           </Col>
           <Col xs={12} className="p-none">
-          {videos.map((video,index) =>
-          index === 0 ? (
-            <Col xs={12} className="video-wrap" key={video.id}>
-              <Col className="video-title">{video.title}</Col>
-              <Col className="video-icons">
-                <Button className="icon-like action-icon">
-                  <span className="fa fa-heart" />
-                </Button>
-                <br />
-                <Button className="icon-timer action-icon">
-                  <span className="fa fa-clock-o" />
-                </Button>
-              </Col>
-              <Player
-                playsInline
-                poster={video.video_thumb}
-                from={video.video_type}
-                src={video.video_url}
+            {videos.map((video, index) =>
+              <Col
+                xs={index === 0 ? 12 : 6}
+                className="video-wrap"
+                key={video.id}
               >
-                <BigPlayButton position="center" />
-              </Player>
-            </Col>
-          )
-           :
-           ( <Col sm={6} className="video-wrap" key={video.id}>
-              <Col className="video-title">{video.title}</Col>
-              <Col className="video-icons">
-                <Button className="icon-like action-icon">
-                  <span className="fa fa-heart" />
-                </Button>
-                <br />
-                <Button className="icon-timer action-icon">
-                  <span className="fa fa-clock-o" />
-                </Button>
+                <Col className="video-title">
+                  {video.title}
+                </Col>
+                <Col className="video-icons">
+                  <Button className="icon-like action-icon">
+                    <span className="fa fa-heart" />
+                  </Button>
+                  <br />
+                  <Button className="icon-timer action-icon">
+                    <span className="fa fa-clock-o" />
+                  </Button>
+                </Col>
+                <Col className="embed-responsive embed-responsive-16by9">
+                  <Iframe
+                    url={video.video_url + '?rel=0&amp;showinfo=0'}
+                    className="embed-responsive-item"
+                    allowFullScreen
+                  />
+                </Col>
               </Col>
-
-              <Player
-                playsInline
-                poster={video.video_thumb}
-                from={video.video_type}
-                src={video.video_url}
-              >
-                <BigPlayButton position="center" />
-              </Player>
-            </Col>
-           )
-        )}
+            )}
           </Col>
         </Grid>
       </div>
--- src/services/Film.js
+++ src/services/Film.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export function getPublishVideos() {
-    return axios.get(process.env.REACT_APP_API_BASE_URL + 'videos/publish');
-  }
+  return axios.get(process.env.REACT_APP_API_BASE_URL + 'videos/publish');
+}
