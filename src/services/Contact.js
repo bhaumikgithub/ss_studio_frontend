8ba01d6e6bf69b81ec8cf1d5e227a9fbb0930837
bhaumik gadani <bhaumikwork@gmail.com>
2017-09-21 10:50:47 +0530

    made common middleware

--- src/services/Contact.js
+++ src/services/Contact.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import axiosInstance from './CustomLoader';
 export function createContactMessage(params) {
   return axios.post(process.env.REACT_APP_API_BASE_URL + 'contact_messages', {
     contact_message: params
@@ -7,7 +7,9 @@ export function createContactMessage(params) {
 }
 
 export function getContactDetails() {
-  return axios.get(process.env.REACT_APP_API_BASE_URL + 'contact_details');
+  return axiosInstance.get(
+    process.env.REACT_APP_API_BASE_URL + 'contact_details'
+  );
 }
 
 export function getActiveServices() {
