47d88a3a80cef954e5af8905924e4a55e6537420
parulkanani <parulk.tps@gmail.com>
2018-10-01 11:41:44 +0530

    dashboard profile completeness

--- src/components/admin/dashboard/Dashboard.jsx
+++ src/components/admin/dashboard/Dashboard.jsx
@@ -12,24 +12,46 @@ export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      profileCompleteness: {}
+      profileCompleteness: {},
+      letsDoLink: "",
+      parentOfNextTask: "",
+      nextTaskName: ""
     };
   }
-  componentWillMount() {
+  // componentWillMount() {
+  //   var self = this;
+  //   ProfileCompletenessService.getProfileCompleteness()
+  //   .then(function(response) {
+  //     var data = response.data;
+  //     self.setState({
+  //       profileCompleteness: data.data.profile_completeness
+  //     });
+  //   })
+  //   .catch(function(error) {
+  //     console.log(error.response);
+  //   });
+  // }
+  componentDidMount(){
     var self = this;
     ProfileCompletenessService.getProfileCompleteness()
     .then(function(response) {
       var data = response.data;
       self.setState({
         profileCompleteness: data.data.profile_completeness
-      });
+      },()=>self.letsDoLink());
     })
     .catch(function(error) {
       console.log(error.response);
     });
   }
+  letsDoLink(){
+    var next_task_info = document.getElementsByClassName('text-red')[0]
+    var parent_name = next_task_info.parentNode.getElementsByClassName('checkbox-heading')[0].textContent
+    var link = next_task_info.href.substring(next_task_info.href.lastIndexOf("/"))
+    this.setState({letsDoLink: link, nextTaskName: next_task_info.text, parentOfNextTask: parent_name})
+  }
   render() {
-    const {profileCompleteness} = this.state
+    const {profileCompleteness, letsDoLink, nextTaskName, parentOfNextTask} = this.state
     return (
       <Col xs={12} className="categories-page-wrap">
         <Col xs={12} className="filter-wrap p-none">
@@ -40,14 +62,13 @@ export default class Dashboard extends Component {
           <div className="categories-table-wrap">
             Profile Completeness  <b>{profileCompleteness && profileCompleteness.percentage}%</b><br></br>
             <p></p>
-            { profileCompleteness && profileCompleteness.parent_accessor && (
               <div>
                 <p>Next Task:</p>
-                <p>{profileCompleteness && profileCompleteness.parent_accessor} > {profileCompleteness && profileCompleteness.next_task_name}
+                <p>{parentOfNextTask} > {nextTaskName}
                   <Button className="btn-orange lets-do-btn">
                     <Link
                       to={
-                        'albums'
+                        letsDoLink
                       }
                       className="admin-login-btn"
                     >
@@ -56,7 +77,6 @@ export default class Dashboard extends Component {
                   </Button>
                 </p>
               </div>
-            )}
             <div className="round">
               <Checkbox
                 name="active-checkbox"
@@ -69,43 +89,44 @@ export default class Dashboard extends Component {
                   <div className="inside" />
                 </div>
               </Checkbox>
-            </div>
-            <Link
-              to={
-                'albums'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "public_album" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.public_album ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Create 1 public album(portfolio)
-            </Link><br></br>
-            <Link
-              to={
-                'albums'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "private_album" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.private_album ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Create 1 private album
-            </Link><br></br>
-            <Link
-              to={
-                'settings'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "watermark" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.watermark ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add Watermark
-            </Link><br></br>
-            <Link
-              to={
-                'albums'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "photo" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.photo ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Upload 1 photo
-            </Link><br></br>
+            
+              <Link
+                to={
+                  'albums'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "public_album" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.public_album ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Create 1 public album(portfolio)
+              </Link><br></br>
+              <Link
+                to={
+                  'albums'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "private_album" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.private_album ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Create 1 private album
+              </Link><br></br>
+              <Link
+                to={
+                  'settings'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "watermark" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.watermark ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add Watermark
+              </Link><br></br>
+              <Link
+                to={
+                  'albums'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "photo" ? "album-management-completeness text-red" : (profileCompleteness.album_management && profileCompleteness.album_management.photo ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Upload 1 photo
+              </Link>
+            </div><br></br>
             <div className="round">
               <Checkbox
                 name="active-checkbox"
-                className="all-selection-check album-status-checkboxes rounded-checkbox"
+                className="album-status-checkboxes rounded-checkbox"
                 checked={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.site_content ? 'checked' : ''}
                 disabled={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.site_content ? true : false}
               >
@@ -114,51 +135,52 @@ export default class Dashboard extends Component {
                   <div className="inside" />
                 </div>
               </Checkbox>
-            </div>
-            <Link
-              to={
-                'site_contents'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "about_us" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.about_us ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              About us
-            </Link><br></br>
-            <Link
-              to={
-                'site_contents'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "service" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.service ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add 1 service
-            </Link><br></br>
-            <Link
-              to={
-                'site_contents'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "contact_us" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.contact_us ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add contact details
-            </Link><br></br>
-            <Link
-              to={
-                'site_contents'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "social_media_link" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.social_media_link ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add 1 social media link
-            </Link><br></br>
-            <Link
-              to={
-                'site_contents'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "website_detail" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.website_detail ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add copyright and site details
-            </Link><br></br>
+            
+              <Link
+                to={
+                  'site_contents'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "about_us" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.about_us ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                About us
+              </Link><br></br>
+              <Link
+                to={
+                  'site_contents'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "service" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.service ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add 1 service
+              </Link><br></br>
+              <Link
+                to={
+                  'site_contents'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "contact_us" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.contact_us ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add contact details
+              </Link><br></br>
+              <Link
+                to={
+                  'site_contents'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "social_media_link" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.social_media_link ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add 1 social media link
+              </Link><br></br>
+              <Link
+                to={
+                  'site_contents'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "website_detail" ? "album-management-completeness text-red" : (profileCompleteness.site_content && profileCompleteness.site_content.website_detail ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add copyright and site details
+              </Link>
+            </div><br></br>
             <div className="round">
               <Checkbox
                 name="active-checkbox"
-                className="all-selection-check album-status-checkboxes rounded-checkbox"
+                className="album-status-checkboxes rounded-checkbox"
                 checked={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.homepage_gallery ? 'checked' : ''}
                 disabled={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.homepage_gallery ? true : false}
               >
@@ -167,19 +189,20 @@ export default class Dashboard extends Component {
                   <div className="inside" />
                 </div>
               </Checkbox>
-            </div>
-            <Link
-              to={
-                'homepage_gallery'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "homepage_gallery_photo" ? "album-management-completeness text-red" : (profileCompleteness.homepage_gallery && profileCompleteness.homepage_gallery.homepage_gallery_photo ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Upload 1 photo on homepage gallery
-            </Link><br></br>
+            
+              <Link
+                to={
+                  'homepage_gallery'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "homepage_gallery_photo" ? "album-management-completeness text-red" : (profileCompleteness.homepage_gallery && profileCompleteness.homepage_gallery.homepage_gallery_photo ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Upload 1 photo on homepage gallery
+              </Link>
+            </div> <br></br>
             <div className="round">
               <Checkbox
                 name="active-checkbox"
-                className="all-selection-check album-status-checkboxes rounded-checkbox"
+                className="album-status-checkboxes rounded-checkbox"
                 checked={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.video_portfolio ? 'checked' : ''}
                 disabled={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.video_portfolio ? true : false}
               >
@@ -188,19 +211,20 @@ export default class Dashboard extends Component {
                   <div className="inside" />
                 </div>
               </Checkbox>
-            </div>
-            <Link
-              to={
-                'video_films'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "youtube_video" ? "album-management-completeness text-red" : (profileCompleteness.video_portfolio && profileCompleteness.video_portfolio.youtube_video ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Upload youtube video
-            </Link><br></br>
+            
+              <Link
+                to={
+                  'video_films'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "youtube_video" ? "album-management-completeness text-red" : (profileCompleteness.video_portfolio && profileCompleteness.video_portfolio.youtube_video ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Upload youtube video
+              </Link>
+            </div><br></br>
             <div className="round">
               <Checkbox
                 name="active-checkbox"
-                className="all-selection-check album-status-checkboxes rounded-checkbox"
+                className="album-status-checkboxes rounded-checkbox"
                 checked={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.testimonial ? 'checked' : ''}
                 disabled={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.testimonial ? true : false}
               >
@@ -209,19 +233,20 @@ export default class Dashboard extends Component {
                   <div className="inside" />
                 </div>
               </Checkbox>
-            </div>
-            <Link
-              to={
-                'testimonials'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "add_testimonial" ? "album-management-completeness text-red" : (profileCompleteness.testimonial && profileCompleteness.testimonial.add_testimonial ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add 1 testimonial
-            </Link><br></br>
+            
+              <Link
+                to={
+                  'testimonials'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "add_testimonial" ? "album-management-completeness text-red" : (profileCompleteness.testimonial && profileCompleteness.testimonial.add_testimonial ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add 1 testimonial
+              </Link>
+            </div><br></br>
             <div className="round">
               <Checkbox
                 name="active-checkbox"
-                className="all-selection-check album-status-checkboxes rounded-checkbox"
+                className="album-status-checkboxes rounded-checkbox"
                 checked={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.contacts ? 'checked' : ''}
                 disabled={profileCompleteness && profileCompleteness.completeness_status && profileCompleteness.completeness_status.contacts ? true : false}
               >
@@ -230,15 +255,16 @@ export default class Dashboard extends Component {
                   <div className="inside" />
                 </div>
               </Checkbox>
-            </div>
-            <Link
-              to={
-                'contacts'
-              }
-              className={profileCompleteness && profileCompleteness.next_task === "contact_details" ? "album-management-completeness text-red" : (profileCompleteness.contacts &&profileCompleteness.contacts.contact_details ? "album-management-completeness text-green" : "album-management-completeness")}
-            >
-              Add contact details
-            </Link><br></br>
+            
+              <Link
+                to={
+                  'contacts'
+                }
+                className={profileCompleteness && profileCompleteness.next_task === "contact_details" ? "album-management-completeness text-red" : (profileCompleteness.contacts &&profileCompleteness.contacts.contact_details ? "album-management-completeness text-green" : "album-management-completeness")}
+              >
+                Add contact details
+              </Link>
+            </div><br></br>
           </div>
         </Col>
       </Col>
