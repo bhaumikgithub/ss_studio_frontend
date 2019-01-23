c9ea35957e5b3ab011df333c024b4f0baaabf5bd
kinjalt <kinjalthakkar.tps@gmail.com>
2019-01-23 16:50:50 +0530

    change the url

--- src/components/layout/admin/Header.js
+++ src/components/layout/admin/Header.js
@@ -66,7 +66,7 @@ export default class Header extends Component {
           <Button className="logout-btn btn btn-orange view-my-website-btn">
             <Link
               to={
-                process.env.REACT_APP_API_BASE_URL + currentUser().alias
+                process.env.REACT_APP_LANDING_URL + currentUser().alias
               }
               target="_blank"
               className="admin-login-btn"
